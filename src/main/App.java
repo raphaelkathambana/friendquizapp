a95c0c6c64fea86ae59bf02ef234262bb1f7922a
Raphael Kathambana <maya12raph@gmail.com>
2023-11-14 20:37:03 +0300

    Add GetConnection instance for SQL connection

--- src/main/App.java
+++ src/main/App.java
@@ -3,9 +3,15 @@ package main;
 import javax.swing.SwingUtilities;
 
 import ui.QuizCreationUI;
+import util.GetConnection;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(QuizCreationUI::new);
+
+        // you should use this to make a new connection
+        GetConnection connection = GetConnection.getInstance();
+        // oh and also don't add any other code to that file...
+        // any other sql related stuff should have it's own class
     }
 }
